5a1c9bedf869c0da0b45e299edde67ffed724ade
WorkShop_Linux <WorkShopLinux@users.noreply.github.com>
2016-04-22 20:29:13 -0300

    Update pelicanconf.py

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,15 +2,17 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = u'2TRCA'
-SITENAME = u'Workshop Linux'
+AUTHOR = '2TRCA'
+SITENAME = 'Workshop Linux'
 SITEURL = ''
 
 PATH = 'content'
 
 TIMEZONE = 'America/Sao_Paulo'
 
-DEFAULT_LANG = u'pt'
+DEFAULT_DATE_FORMAT = '%Y-%m-%d'
+DEFAULT_DATE = 'fs'
+DEFAULT_LANG = 'pt'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -18,6 +20,24 @@ CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
+
+STATIC_PATHS = ['image',
+                'static',
+                'extra/sitelogo.png',
+                'extra/favicon.ico',
+                'extra/favicon.png',
+                'extra/robots.txt',
+            #    'extra/google331456191962689c',
+                'extra/CNAME']
+EXTRA_PATH_METADATA = {
+                        'extra/sitelogo.png': {'path': 'sitelogo.png'},
+                        'extra/robots.txt': {'path': 'robots.txt'},
+                        'extra/favicon.ico': {'path': 'favicon.ico'},
+                        'extra/favicon.png': {'path': 'favicon.png'},
+                 #       'extra/google331456191962689c': {'path': 'google331456191962689c.html'},
+                        'extra/CNAME': {'path': 'CNAME'},
+                      }
+
 
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
