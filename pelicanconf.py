9aa4c6a63b2c1d604ca70931cee8f446cf63fbd6
Victor Apolonio <victorapolonio@live.com>
2016-06-02 12:54:34 -0300

    Update pelicanconf.py
    removendo cod analytics, fazer trackeamento com gtm

--- pelicanconf.py
+++ pelicanconf.py
@@ -75,16 +75,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-5QPCMF');</script>
 <!-- End Google Tag Manager -->distribuições'''
-NAV_LINK_2 = '''SOBRE<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-78722919-1', 'auto');
-  ga('send', 'pageview');
-
-</script>'''
+NAV_LINK_2 = 'SOBRE'
 NAV_LINK_3 = 'BLOG'
 NAV_LINK_4 = 'TRABALHOS'
 TI_LINK_4 = 'Trabalhos'
